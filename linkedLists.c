e7847f00862387c8f420bedd909098724c4602a0
jmkc <jacquelinemiriam@gmail.com>
2014-11-29 22:49:02 -0500

    Completed llist 1 variation linked list main operations

--- linkedLists.c
+++ linkedLists.c
@@ -7,26 +7,110 @@ Typedef for all variations
 *************************************/
 
 //Link1
+typedef struct{
+    int value;
+    struct Link *next;
+} Link;
 //LList1
-//Link2
+typedef struct{
+    int size;
+    Link *head;
+}LList1;
 //LList2
-//Link3
+typedef struct{
+    int size;
+    Link *head;
+    Link *tail;
+}LList2;
 //LList3
-//Link4
+typedef struct{
+    int size;
+    Link *head;
+}LList3;
 //LList4
+typedef struct{
+    int size;
+    Link *tail;
+}LList4;
+
 
 /*************************************
 Main Operations - Variation 1 
 *************************************/
 
-void prepend(Link1 *l, LList1 *ll); //Prepend a link
+void prepend(Link *l, LList1 *ll); //Prepend a link
+{
+    l->next = ll->head;
+    ll->head = l;
+    ll->size++;
+}
 
-void append(Link1 *l, LList1 *ll); //Append a link iteratively
+void append(Link *l, LList1 *ll); //Append a link iteratively
+{
+    Link lastLink = ll->head;
+    while(lastLink->next != NULL)
+    {
+        lastLink = lastLink->next;
+    }
+    lastLink->next = l;
+    ll->size++;
+}
 
-void appendRecursive(Link1 *l, LList1 *ll); //Append a link iteratively
+void appendRecursive(Link *l, LList1 *ll); //Append a link iteratively
 
-Link1 *removeFront(LList1 *ll); //Remove front and return link
+Link *removeFront(LList1 *ll); //Remove front and return link
+{
+    Link *link = NULL;
+    if(ll!= NULL)
+    {
+        link = ll->head;
+        if (link != NULL)
+        {
+            ll->head = link->next;
+            ll->size--;
+        }
+    }
+    return (link);
+}
+Link *removeEnd(LList1 *ll); //Remove last and return link
+{
+    Link *link1 = NULL, *link2 = NULL, *resultLink = NULL;
+    if(ll != NULL)
+    {
+        link1 = ll->head;
+        if(link1 != NULL)
+        {
+            link2 = link1->next;
+            if (link2 != NULL)
+            {
+                while(link2->next != NULL)
+                {
+                    link1 = link2;
+                    link2 = link1->next;
+                }
+                link1->next = NULL;
+                resultLink = link2;
+            } else
+            {
+                resultLink = link1;
+            }
+        }
+    }
+    return (resultLink);
+}
 
-Link1 *removeEnd(LList1 *ll); //Remove last and return link
+int count(Link *l); //Count links in a list
+{
+    if (l->next != NULL)
+    {
+        return(count(l->next)+1);
+    }
+    return (0);
+}
 
-int count(Link1 *l); //Count links in a list
+
+
+
+
+
+
